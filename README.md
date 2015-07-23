9ac7074444733b41087f51a89e5604553e0b38b2
iMumuMua <imumu@foxmail.com>
2015-07-23 22:43:03 +0800

    重新设计

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # x-module
-Modularization programming for JavaScript.
+`x-module` is a light weight library for JavaScript modularization programming. You could use it in both browser and node.js/io.js.
+
+## Start
+### In browser:
+First step, add `<script>` tag for `x-module.js` in your web page, such as `index.html`:
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <!--content-->
+    <script src="path/for/x-module/x-module.js"></script>
+    <script src="module-a.js"></script>
+    <script src="module-b.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
+```
+
+And then add some modules:
+
+module-a.js:
+```js
+xModule.def('moduleA', function() {
+    return 'A';
+});
+```
+
+module-b.js:
+```js
+xModule.def('moduleB', ['moduleA'], function(A) {
+    return function(name) {
+        return 'Hello, ' + name + "! I'm moduleB. The moduleA is " + A;
+    };
+});
+```
+
+Run the module:
+
+main.js:
+```js
+xModule.run('moduleB', function(B) {
+    B('world'); // Hello, world! I'm moduleB. The moduleA is A
+});
+```
+
+### In node.js or io.js:
+
+Define modules:
+
+module-a.js:
+```js
+xModule = require('x-module');
+xModule.def('moduleA', function() {
+    return 'A';
+});
+```
+
+Run:
+
+main.js:
+```js
+xModule = require('x-module');
+require('./module-a.js');
+xModule.run('moduleA', function(A) {
+    console.log(A); // 'A'
+});
+```
+
+## Why x-module?
+* **easy**: Just concat and uglify js files in browser, or require files in `node.js`/`io.js`. You don't need to care about the order of files.
+* **consistency**: The browser client and server side in the same way to define the module.
+
+## License
+[MIT](./LICENSE)
--- test/module.js
+++ test/module.js
@@ -1,65 +1,41 @@
-var x = require('../lib/x-module');
+var xModule = require('../lib/x-module');
 
-x.def('sync', [], function() {
-    var mod = function() {
-        return 'sync';
+xModule.def('moduleA', function() {
+    return 'A'
+});
+
+xModule.def('moduleB', ['moduleA'], function(A) {
+    return function() {
+        return 'B' + A;
     };
-    this.pub(mod);
-});
-x.def('syncMain', ['sync'], function(sync) {
-    sync().should.equal('sync');
-    this.pub();
-});
-x.def('async', [], function() {
-    var self = this;
-    var mod = function() {
-        return 'async';
-    };
-    setTimeout(function() {
-        self.pub(mod);
-    });
-});
-x.def('asyncMain', ['async'], function(async) {
-    async().should.equal('async');
-    this.pub();
-});
-x.def('mix', ['sync', 'async'], function(sync, async) {
-    sync().should.equal('sync');
-    async().should.equal('async');
-    this.pub();
-});
-x.def('nest', ['syncMain', 'asyncMain'], function(sync, async) {
-    this.pub();
 });
 
-describe('sync module', function() {
-    it('should work', function(done) {
-        x.run('syncMain').then(function() {
-            done();
-        }, done);
-    });
+xModule.def('moduleC', ['moduleA', 'moduleB'], function(A, B) {
+    return A + B();
 });
 
-describe('async module', function() {
-    it('should work', function(done) {
-        x.run('asyncMain').then(function() {
-            done();
-        }, done);
+describe('xModule', function() {
+    describe('single module', function() {
+        it('should run', function() {
+            xModule.run('moduleA', function(A) {
+                A.should.equal('A');
+            });
+        });
     });
-});
 
-describe('mix module', function() {
-    it('should work', function(done) {
-        x.run('mix').then(function() {
-            done();
-        }, done);
+    describe('dep module', function() {
+        it('should run', function() {
+            xModule.run('moduleB', function(B) {
+                B().should.equal('BA');
+            });
+        });
     });
-});
 
-describe('nested module', function() {
-    it('should work', function(done) {
-        x.run('nest').then(function() {
-            done();
-        }, done);
+    describe('multi dep module', function() {
+        it('should run', function() {
+            xModule.run('moduleC', function(C) {
+                C.should.equal('ABA');
+            });
+        });
     });
 });
